8d47b562c36f73981f9fee5c53b9ee2eed153d84
IT-agencyAtom <59134588+IT-agencyAtom@users.noreply.github.com>
2024-10-11 15:10:14 +0700

    Add Custom Generation application (#3)
    - Add Custom Generation application
    - Set generation preferences from code
    - Add SetTargetDirectory and SetProjectPath methods

---
+++ RunCodeGenerator/GeneratorHelpers.cs
@@ -0,0 +1,22 @@
+﻿using DesperateDevs.Serialization;
+
+namespace MyProject.CodeGenerator
+{
+    public static class GeneratorHelpers
+    {
+        public static void SetTargetDirectory(this Preferences preferences, string targetDirectory)
+        {
+            preferences["Jenny.Plugins.TargetDirectory"] = targetDirectory;
+        }
+
+        public static void SetContexts(this Preferences preferences, string contexts)
+        {
+            preferences["Entitas.CodeGeneration.Plugins.Contexts"] = contexts;
+        }
+
+        public static void SetProjectPath(this Preferences preferences, string projectPath)
+        {
+            preferences["Jenny.Plugins.ProjectPath"] = projectPath;
+        }
+    }
+}
